996acbc475b63d33d7f6c28963b4db89b5d7cd94
Mike Ortiz <emike108@aim.com>
2024-07-01 00:03:43 -0700

    initial changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,71 +1,45 @@
-import React from 'react';
-import PokemonEntry from './PokemonEntry.jsx';
-import PokemonList from './PokemonList.jsx';
 import axios from 'axios';
+import React, { useState } from 'react';
+import { PokemonList } from './PokemonList.jsx';
 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      display: []
-    }
-    this.showAll = this.showAll.bind(this);
-  }
+export function App() {
+  const [display, setDisplay] = useState([]);
 
-  showAll() {
+  function showAll() {
     axios.get('/pokemon')
-    .then( results => {
+    .then(results => {
       console.log(results.data)
-      this.setState({
-        display: results.data
-      })
+      setDisplay(results.data)
     })
     .catch(err => console.error(err))
   }
 
-  render() {
-    return (
+  return (
+    <div>
       <div>
+        <h1>Pokemon!</h1>
+        <button onClick={showAll}>Show All</button>
+        <select id="type">
+          <option>Sort by Type</option>
+          <option>Grass</option>
+          <option>Fire</option>
+          <option>Water</option>
+          <option>Normal</option>
+          <option>Poison</option>
+          <option>Electric</option>
+          <option>Ground</option>
+          <option>Fighting</option>
+          <option>Psychic</option>
+          <option>Rock</option>
+          <option>Ghost</option>
+          <option>Dragon</option>
+        </select>
+        <button>INSERT</button>
         <div>
-          <h1>Pokemon!</h1>
-          <button onClick={this.showAll}>Show All</button>
-          <select id="type">
-            <option>Sort by Type</option>
-            <option>Grass</option>
-            <option>Fire</option>
-            <option>Water</option>
-            <option>Normal</option>
-            <option>Poison</option>
-            <option>Electric</option>
-            <option>Ground</option>
-            <option>Fighting</option>
-            <option>Psychic</option>
-            <option>Rock</option>
-            <option>Ghost</option>
-            <option>Dragon</option>
-          </select>
-          <button>INSERT</button>
-          <div>
-            <PokemonList pokemons={this.state.display} />
-          </div>
+          <PokemonList pokemons={display} />
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-export default App;
-
-
-/*
-<div>
-    <h3>Ivysaur</h3>
-    <img src="http://vignette3.wikia.nocookie.net/nintendo/images/8/86/Ivysaur.png/revision/latest?cb=20141002083450&path-prefix=en" />
-  </div>
-  <div>
-    <h3>Venusaur</h3>
-    <img src="http://vignette2.wikia.nocookie.net/nintendo/images/b/be/Venusaur.png/revision/latest?cb=20141002083423&path-prefix=en" />
-  </div>
-</div>
-*/
--- client/src/components/PokemonList.jsx
+++ client/src/components/PokemonList.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import PokemonEntry from './PokemonEntry.jsx';
+import { PokemonEntry } from './PokemonEntry.jsx';
 
 
-const PokemonList = (props) => (
-  <div>
-    {props.pokemons.map((pokemon, index) => (
-    <PokemonEntry pokemon={pokemon} key={index}/>
-    ))}
-  </div>
-)
-
-export default PokemonList;
+export function PokemonList(props) {
+  return (
+    <div>
+      {props.pokemons.map((pokemon, index) => (
+      <PokemonEntry pokemon={pokemon} key={index}/>
+      ))}
+    </div>
+  )
+}
