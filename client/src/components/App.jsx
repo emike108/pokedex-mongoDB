c2a667c1bd99326ec4bee1908a6ee171e1ab8178
Dom Burnham <dominick.burnham@gmail.com>
2024-07-12 09:17:49 -0700

    incorporated filter functionality

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,15 +4,28 @@ import { PokemonList } from "./PokemonList.jsx";
 
 export function App() {
   const [display, setDisplay] = useState([]);
+  const [type, setType] = useState(""); //chatGPT caught that I had set this to an array, rather than a string
 
   function showAll() {
     axios
       .get("/pokemon")
       .then((results) => {
+        if (type === "" || type === "Sort by Type") {
+          setDisplay(results.data);
+        } else {
+          setDisplay(
+            results.data.filter((eachPokemon) => eachPokemon.type === type)
+          );
+        }
         console.log(results.data);
-        setDisplay(results.data);
       })
       .catch((err) => console.error(err));
+  }
+
+  function handleClick(Event) {
+    const { value } = Event.target;
+    setType(value);
+    console.log(value);
   }
 
   return (
@@ -20,20 +33,20 @@ export function App() {
       <div>
         <h1>Pokemon!</h1>
         <button onClick={showAll}>Show All</button>
-        <select id="type">
-          <option>Sort by Type</option>
-          <option>Grass</option>
-          <option>Fire</option>
-          <option>Water</option>
-          <option>Normal</option>
-          <option>Poison</option>
-          <option>Electric</option>
-          <option>Ground</option>
-          <option>Fighting</option>
-          <option>Psychic</option>
-          <option>Rock</option>
-          <option>Ghost</option>
-          <option>Dragon</option>
+        <select id="type" value={type} onChange={handleClick}>
+          <option value="Sort by Type">Sort by Type</option>
+          <option value="Grass">Grass</option>
+          <option value="Fire">Fire</option>
+          <option value="Water">Water</option>
+          <option value="Normal">Normal</option>
+          <option value="Poison">Poison</option>
+          <option value="Electric">Electric</option>
+          <option value="Ground">Ground</option>
+          <option value="Fighting">Fighting</option>
+          <option value="Psychic">Psychic</option>
+          <option value="Rock">Rock</option>
+          <option value="Ghost">Ghost</option>
+          <option value="Dragon">Dragon</option>
         </select>
         <button>INSERT</button>
         <div>
